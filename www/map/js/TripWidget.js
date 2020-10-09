298b4627908678690d01f86205bbc246e6597f2d
Gus Teran <ateran@wpi.edu>
2020-10-09 10:30:48 -0400

    Add frontend of trip info

--- www/map/js/TripWidget.js
+++ www/map/js/TripWidget.js
@@ -34,6 +34,26 @@ define([
       lang.mixin(this, defaults, parameters);
       this.union = false;
       this.currentGeometry = null;
+      this.trips = [];
+      this.trips.push({
+        name: "Northern Circle",
+        author: 0,
+        id: 0,
+        segments: "",
+        review: null,
+        isPublished: false,
+        popularity: 0
+      });
+      this.user = {
+        id: 0,
+        name: "Default"
+      }
+      this.tripElements = [];
+      this.tripElements.push(dom.byId("model-trip"));
+
+      on(dom.byId("model-trip"), "click", lang.hitch(this, function (){
+        console.log("Northern Circle, ya clicked it");
+      }));
     },
 
     /* Public Methods */
@@ -44,6 +64,11 @@ define([
         scalebarUnits: this.scalebarUnits,
         chartOptions: this.chartParams
       };
+
+      var createTripBtn = dom.byId("createTripBtn");
+      on(createTripBtn, "click", lang.hitch(this, function () {
+        this._createTrip();
+      }));
 
       console.log("Setup Trip Widget");
 
@@ -60,9 +85,13 @@ define([
         var sel = this.map.infoWindow.getSelectedFeature();
         if(sel && sel.attributes){
           console.log("Selection is");
-          console.log(sel.attributes.TRLNAME);
+          console.log(sel);
+          console.log(this.map._layers["2019_GLAC_Trails_4829"]);
+          console.log(this.map.infoWindow);
+
         }
       }));
+
 
     },
     toggleUnion: function () {
@@ -72,11 +101,37 @@ define([
     testJsonRest: function (JsonRest) {
       this.store = new JsonRest({target: DATABASE_LINK});
       this.store.put({
-        trips: "please",
-        likedTrips: "work"
+        name: "TestName",
+        likedTrips: "Test",
+        trips: "Test"
       });
       // fetch(DATABASE_LINK)
       // .then(data => console.log(data));
+    },
+    _createTrip: function () {
+      let newTrip = {
+        author: this.user.id,
+        id: this.trips.length,
+      };
+
+      console.log("Create a trip of id:"+newTrip.id);
+
+      this.trips.push(newTrip);
+      this.tripElements.push(dom.byId(""+newTrip.id));
+
+      let tripElement = this.tripElements[newTrip.id];
+      dom.byId("trip-"+newTrip.id).classList.remove("hide");
+      on(tripElement, "click", lang.hitch(this, function (){
+        console.log("Click on trip of id: "+newTrip.id);
+      }));
+
+      console.log(this.trips);
+
+      if(this.trips.length > 5){
+        dom.byId("createTripBtn").classList.add("hide");
+        console.log("Hide create trip");
+      }
+
     }
   });
 });
