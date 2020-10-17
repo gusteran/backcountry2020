fb5d3f143963812e607e924727685886cb1bfa80
Gus Teran <ateran@wpi.edu>
2020-10-17 14:55:34 -0400

    Server tweaks

--- server/server.js
+++ server/server.js
@@ -6,15 +6,15 @@ const path = require("path");
 
 const port = 3000;
 
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var keyPath = path.resolve(__dirname, './sslcert/rootCA.key');
-var privateKey  = fs.readFileSync(keyPath, 'utf8');
-var certPath = path.resolve(__dirname, './sslcert/rootCA.pem');
-var certificate = fs.readFileSync(certPath, 'utf8');
+// var fs = require('fs');
+// var http = require('http');
+// var https = require('https');
+// var keyPath = path.resolve(__dirname, './sslcert/rootCA.key');
+// var privateKey  = fs.readFileSync(keyPath, 'utf8');
+// var certPath = path.resolve(__dirname, './sslcert/rootCA.pem');
+// var certificate = fs.readFileSync(certPath, 'utf8');
 
-var credentials = {key: privateKey, cert: certificate};
+// var credentials = {key: privateKey, cert: certificate};
 
 app.use(cors());
 
@@ -35,11 +35,11 @@ app.get('/', (req, res) => {
 require("./app/routes/user.routes.js")(app);
 require("./app/routes/trip.routes.js")(app);
 
-// app.listen(port, () => console.log('Starting server on port '+port));
+app.listen(port, () => console.log('Starting server on port '+port));
 // your express configuration here
 
 // var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
+// var httpsServer = https.createServer(credentials, app);
 
 // httpServer.listen(port, () => console.log('Starting server on port '+port));
-httpsServer.listen(port, () => console.log('Starting SSL server on port '+port));
+// httpsServer.listen(port, () => console.log('Starting SSL server on port '+port));
